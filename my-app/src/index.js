3f6305cadcdd5dd1d0c10a79d2d1c354e2eb9b52
1010jms <1010jms@gmail.com>
2018-03-01 18:38:05 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class TandCSurf extends React.Component {
 
   render() {
     return (
-        <h1>T&C Surf!</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>T&C Surf!</Header>
+        </Container>
+    );
   }
 }
 
