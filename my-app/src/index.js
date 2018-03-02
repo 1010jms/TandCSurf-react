6e0aa02ec98288af15b9afc2ff30330bbf473965
1010jms <1010jms@gmail.com>
2018-03-01 18:32:38 -1000

    Task 2

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class TandCSurf extends React.Component {
+
+  render() {
+    return (
+        <h1>T&C Surf!</h1>
+  );
+  }
+}
+
+ReactDOM.render(<TandCSurf />, document.getElementById('root'));
